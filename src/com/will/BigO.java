b18dd782b8aff1c21b93c47f7a28226a053fe597
Will brown <wtbrown10@gmail.com>
2021-09-29 21:00:40 -0400

    first commit

---
+++ src/com/will/BigO.java
@@ -0,0 +1,13 @@
+package com.will;
+
+public class BigO {
+
+    public static void main(String[] args) {
+
+
+//a big O notation gives us a way of comparing
+//the time complexity of different algorithms
+//in an objective manner, in a hardware-independent manner.
+
+    }
+}
